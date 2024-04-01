eb01731afdda103c8905db632a577f50e671a914
Rocco Gibbons <roccogibbons@outlook.com>
2024-04-01 22:16:32 +0100

    Finished plurality

--- problemset3/plurality/plurality.c
+++ problemset3/plurality/plurality.c
@@ -2,17 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-int shorten(char string[]);
-void sort(int sorted[], int argc, int voters);
+int lengthOfString(char string[]);
+void sort(int array[], int length);
 
 int main(int argc, char **argv){
     if(argc == 1){
-        printf("./plurality.exe candidate1 candidate2....");
+        printf("Usage: ./plurality [candidate ...]");
     }
+
     else{
-        int numberOfVotesForCandidate[argc];
-        for(int i = 0; i < argc; i++){
-            numberOfVotesForCandidate[i] = 0;
+        int amountOfCandidates = argc - 1;
+
+        char* candidateList[amountOfCandidates];
+        for(int i = 1; i < argc; i++){
+            candidateList[i-1] = argv[i];
+        }
+
+        int candidateVotes[amountOfCandidates];
+        for(int i = 0; i < amountOfCandidates; i++){
+            candidateVotes[i] = 0;
         }
 
         int voters;
@@ -26,12 +34,13 @@ int main(int argc, char **argv){
             printf("Vote: ");
             scanf("%s", vote);
 
-            int length = shorten(vote);
+            int length = lengthOfString(vote);
             vote = (char*) realloc(vote, length);
 
-            for(int j = 1; j < argc; j++){
-                if(strcmp(vote, argv[j]) == 0){
-                    numberOfVotesForCandidate[j]++;
+            for(int j = 0; j < amountOfCandidates; j++){
+                if(strcmp(vote, candidateList[j]) == 0){
+                    candidateVotes[j]++;
+                    break;
                 }
                 else if(j == argc - 1){
                     printf("Invalid vote\n");
@@ -39,23 +48,28 @@ int main(int argc, char **argv){
             }
 
             free(vote);
+        }
+        
+        int sorted[amountOfCandidates];
+
+        for(int i = 0; i < amountOfCandidates; i++){
+            sorted[i] = candidateVotes[i];
+        }
+
+        sort(sorted, amountOfCandidates);
+
+        int biggest = sorted[amountOfCandidates-1];
+
+        for(int i = 0; i < amountOfCandidates; i++){
+            if(candidateVotes[i] == biggest){
+                printf("%s\n", candidateList[i]);
             }
-
-        int sorted[argc];
-        for(int i = 0; i < argc; i++){
-            numberOfVotesForCandidate[i] = sorted[i];
         }
-        sort(sorted, argc, voters);
-
-        for(int i = 0; i < argc; i++){
-            printf("%i ", sorted[i]);
-        }
-
     }
     return 0;
 }
 
-int shorten(char string[]){
+int lengthOfString(char string[]){
     for(int i = 0; i <= 100; i++){
         if(string[i] == '\000'){
             return i;
@@ -64,12 +78,19 @@ int shorten(char string[]){
     return -1;
 }
 
-void sort(int sorted[], int argc, int voters){
-    for(int i = 0; i < argc-1; i++){
-        if(sorted[i] > sorted[i+1]){
-            int temp = sorted[i];
-            sorted[i] = sorted[i+1];
-            sorted[i+1] = temp;
+void sort(int array[], int length) {
+    for(int i = 0; i < length - 1; ++i) { 
+        int swapped = 0;
+        for (int j = 0; j < length - i - 1; ++j) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                swapped = 1;
+            }
         }
+    if (swapped == 0) {
+      break;
     }
+  }
 }
